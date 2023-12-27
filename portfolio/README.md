d4c4afe34e33b6bc558d3550275bd8b50e98f1bb
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2023-12-27 11:02:56 +0530

    frontend

---
+++ portfolio/README.md
@@ -0,0 +1,2 @@
+this is my text parser application just created for basic revison of basic concepts # Text-Parser-Application
+# Text-Parser-Application
---
+++ portfolio/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import FormData from './components/TextParser';
+function App() {
+  return (
+    <>
+      <NavBar title='Text Parser'/>
+      <FormData/>
+    </>
+  );
+}
+
+export default App;
